ab7b9419909b5c1b7081887edcf5ff5d453d138f
Mans Matulewicz <mans.matulewicz@gmail.com>
2018-10-05 15:15:10 +0200

    making code work for arm linux

--- flux-builder/build.sh
+++ flux-builder/build.sh
@@ -2,7 +2,9 @@ cd /go/src/github.com/weaveworks/ && \
 git clone https://github.com/weaveworks/flux.git && \
 cd flux && \
 git checkout 1.7.1 && \
-sed -i 's#int8)#uint8)#g' checkpoint/checkpoint_linux.go
+cp checkpoint/checkpoint_linux.go checkpoint/checkpoint_linux_arm.go
+mv checkpoint/checkpoint_linux.go checkpoint/checkpoint_linux_amd64.go
+sed -i 's#int8)#uint8)#g' checkpoint/checkpoint_linux_arm.go
 sed -i "s/amd64/arm/g" Makefile && \
 dep ensure && \
 make
